9009c2416762180d559f2129a54df359f9192e3d
Alex <robertalexwymer@gmail.com>
2020-10-09 18:44:53 -0400

    Add project files.

---
+++ src/StartScene.cpp
@@ -0,0 +1,83 @@
+#include "StartScene.h"
+#include <algorithm>
+#include "Game.h"
+#include "glm/gtx/string_cast.hpp"
+#include "EventManager.h"
+
+StartScene::StartScene()
+{
+	StartScene::start();
+}
+
+StartScene::~StartScene()
+= default;
+
+void StartScene::draw()
+{
+
+	TextureManager::Instance()->draw("StartScene", 400, 300, 0, 255, true);
+
+	drawDisplayList();
+}
+
+void StartScene::update()
+{
+	updateDisplayList();
+}
+
+void StartScene::clean()
+{
+	removeAllChildren();
+}
+
+void StartScene::handleEvents()
+{
+	EventManager::Instance().update();
+
+	// Keyboard Events
+	if(EventManager::Instance().isKeyDown(SDL_SCANCODE_ESCAPE))
+	{
+		TheGame::Instance()->quit();
+	}
+
+	if(EventManager::Instance().isKeyDown(SDL_SCANCODE_1))
+	{
+		TheGame::Instance()->changeSceneState(PLAY_SCENE);
+	}
+}
+
+void StartScene::start()
+{
+
+	TextureManager::Instance()->load("../Assets/textures/Background.png", "StartScene");
+
+	const SDL_Color blue = { 0, 0, 255, 255 };
+	
+	m_pInstructionsLabel = new Label("Press 1 to Play", "Consolas", 40, blue, glm::vec2(400.0f, 520.0f));
+	m_pInstructionsLabel->setParent(this);
+	addChild(m_pInstructionsLabel);
+
+	// Start Button
+	m_pStartButton = new Button();
+	m_pStartButton->getTransform()->position = glm::vec2(400.0f, 400.0f); 
+
+	m_pStartButton->addEventListener(CLICK, [&]()-> void
+	{
+		m_pStartButton->setActive(false);
+		TheGame::Instance()->changeSceneState(PLAY_SCENE);
+	});
+	
+	m_pStartButton->addEventListener(MOUSE_OVER, [&]()->void
+	{
+		m_pStartButton->setAlpha(128);
+	});
+
+	m_pStartButton->addEventListener(MOUSE_OUT, [&]()->void
+	{
+		m_pStartButton->setAlpha(255);
+	});
+	addChild(m_pStartButton);
+
+	
+}
+
